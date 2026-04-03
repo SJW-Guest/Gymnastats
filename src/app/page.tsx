30ce9262b0a8f6358e9a93edd219d23f84122766
WSM-sjwinter <sjwinterranch@gmail.com>
2026-04-03 13:28:13 -0500

    Clean build - simplified files

---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main style={{fontFamily:'sans-serif',padding:'2rem',textAlign:'center'}}><h1>Gymnastats</h1><p>MAGA Gymnastics Scoring Platform</p></main>
+}
